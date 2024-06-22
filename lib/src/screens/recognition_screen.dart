606e88d39377759680513fd6f09c7b86dc1984b7
Pavel.Kudelko <pavel.kudelko@student.lab.fi>
2024-06-22 17:01:51 +0300

    Refactor: separate image processing logic into new file

--- lib/src/screens/recognition_screen.dart
+++ lib/src/screens/recognition_screen.dart
@@ -1,10 +1,7 @@
-import 'dart:developer';
-
-import 'package:danfoss_mobile/src/widgets/dialog_window.dart';
 import 'package:flutter/material.dart';
 import 'package:google_mlkit_text_recognition/google_mlkit_text_recognition.dart';
 
-import '../utils/extract_serial_number.dart';
+import '../services/process_image_class.dart';
 
 class RecognizePage extends StatefulWidget {
   final String? path;
@@ -26,63 +23,38 @@ class _RecognizePageState extends State<RecognizePage> {
 
     final InputImage inputImage = InputImage.fromFilePath(widget.path!);
 
-    processImage(inputImage);
+    processImageWrapper(inputImage);
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text("recognition page")),
+ @override
+Widget build(BuildContext context) {
+  Widget bodyContent;
 
-      body: _isBusy == true ? const Center(
-        child: CircularProgressIndicator(), 
-      ) : Container(
-        padding: const EdgeInsets.all(20),
-        child: Text(
-          controller.text
-        )
-      )
+  if (_isBusy) {
+    bodyContent = const Center(child: CircularProgressIndicator());
+  } else {
+    bodyContent = Container(
+      padding: const EdgeInsets.all(20),
+      child: Text(controller.text),
     );
-
   }
-  
-  void processImage(InputImage image) async {
-    final textRecognizer = TextRecognizer(script: TextRecognitionScript.latin);
+  return Scaffold(
+    appBar: AppBar(title: const Text("Recognition Page")),
+    body: bodyContent,
+  );
+}
 
-    void _showDialog(BuildContext context) {
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return DialogError(); 
-      },);}
 
+    void processImageWrapper(InputImage image) async {
     setState(() {
       _isBusy = true;
     });
 
-  try {
-      final RecognizedText recognizedText = 
-      await textRecognizer.processImage(image);
-      String? extractedText = extractSerialNumber(recognizedText.text);
-
-      if (extractedText != null) {
-        controller.text = extractedText;
-      } else {
-        // Handle case where extraction fails
-        _showDialog(context);
-        controller.text = "Can't recognize";
-      }
-    } catch (e) {
-      // Handle any errors that occur during recognition
-      log("Text recognition error: $e");
-      _showDialog(context);
-      controller.text = "Can't recognize";
-    }
-    finally {
-      // end busy state
+    String result = await processImage(context, image);
     setState(() {
+      controller.text = result;
       _isBusy = false;
     });
-    }
   }
+  
 }
