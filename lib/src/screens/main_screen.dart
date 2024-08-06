445aaf3b677229292fdbd10c514dc8c108e1e621
Sari <s.meuronen@me.com>
2024-08-06 08:26:54 +0300

    Changed back-button to x-icon in add manually. Made adjustments to font size  in buttons.

--- lib/src/screens/main_screen.dart
+++ lib/src/screens/main_screen.dart
@@ -31,120 +31,151 @@ class Home extends StatelessWidget {
       appBar: CustomAppBar(),
       // Main body Container, inside there is Column-widget with three buttons.
       body: Center(
-          child: Container(
-              color: const Color.fromRGBO(255, 255, 255, 1),
-              child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  // Here are all three buttons
-                  children: <Widget>[
-                    danfoss.FrontPageButton(
-                        onPressed: () async {
-                          if (Platform.isIOS) {
-                            // permission request here
-                            await cameraPermissionService
-                                .requestPermission(context);
-                          }
-
-                          // log for debug
-                          log("camera");
-                          pickImage(source: ImageSource.camera).then((value) {
-                            if (value != '') {
-                              imageCropperView(value, context).then((value) {
-                                if (value != '') {
-                                  Navigator.push(
-                                      context,
-                                      CupertinoDialogRoute(
-                                          builder: (_) => RecognizePage(
-                                                path: value,
-                                              ),
-                                          context: context));
-                                }
-                              });
-                            }
-                          });
-                        },
-                        buttonText: 'Scan'),
-                    danfoss.FrontPageButton(
-                        onPressed: () async {
-                          if (Platform.isIOS) {
-                            // permission request here
-                            await galleryPermissionService
-                                .requestPermission(context);
-                          }
-                          // log for debug
-                          log("gallery");
-                          pickImage(source: ImageSource.gallery).then((value) {
-                            if (value != '') {
-                              imageCropperView(value, context).then((value) {
-                                if (value != '') {
-                                  Navigator.push(
-                                      context,
-                                      CupertinoDialogRoute(
-                                          builder: (_) => RecognizePage(
-                                                path: value,
-                                              ),
-                                          context: context));
-                                }
-                              });
-                            }
-                          });
-                        },
-                        buttonText: 'Choose from Gallery'),
-                    danfoss.FrontPageButton(
-                        onPressed: () {
-                          showDialog(
-                              context: context,
-                              builder: (_) => AlertDialog(
-                                  content: Container(
-                                      width: 700,
-                                      child: Column(
-                                        mainAxisSize: MainAxisSize.min,
-                                        children: [
-                                          TextField(
-                                            controller: TextEditingController(),
-                                            autofocus: true,
-                                            decoration: InputDecoration(
-                                              hintText:
-                                                  'Insert serial number...',
-                                              border: OutlineInputBorder(
-                                                borderRadius:
-                                                    BorderRadius.circular(8),
-                                                borderSide: BorderSide(
-                                                    color: Colors.redAccent),
-                                              ),
-                                              prefixIcon: Icon(Icons.search),
-                                            ),
-                                            textInputAction:
-                                                TextInputAction.search,
-                                            onSubmitted: (value) {
-                                              Navigator.push(
-                                                  context,
-                                                  CupertinoDialogRoute(
-                                                      builder: (_) =>
-                                                          RecognizePage(
-                                                            serial: value,
-                                                          ),
-                                                      context: context));
-                                            },
-                                          ),
-                                          SizedBox(height: 10),
-                                          Align(
-                                            alignment: Alignment.bottomRight,
-                                            child: danfoss.BackButton(),
-                                          )
-                                        ],
-                                      ))));
-                        },
-                        buttonText: 'Add Manually'),
-                        danfoss.FrontPageButton(
-                          onPressed: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(builder: (context) => HistoryPage()),
-                              );},
-                              buttonText: 'History',
+        child: Container(
+          color: const Color.fromRGBO(255, 255, 255, 1),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            // Here are all three buttons
+            children: <Widget>[
+              danfoss.FrontPageButton(
+                onPressed: () async {
+                  if (Platform.isIOS) {
+                    // permission request here
+                    await cameraPermissionService.requestPermission(context);
+                  }
+                  // log for debug
+                  log("camera");
+                  pickImage(source: ImageSource.camera).then((value) {
+                    if (value != '') {
+                      imageCropperView(value, context).then((value) {
+                        if (value != '') {
+                          Navigator.push(
+                            context,
+                            CupertinoDialogRoute(
+                              builder: (_) => RecognizePage(
+                                path: value,
                               ),
-                  ]))),
+                              context: context,
+                            ),
+                          );
+                        }
+                      });
+                    }
+                  });
+                },
+                buttonText: 'Scan',
+              ),
+              danfoss.FrontPageButton(
+                onPressed: () async {
+                  if (Platform.isIOS) {
+                    // permission request here
+                    await galleryPermissionService.requestPermission(context);
+                  }
+                  // log for debug
+                  log("gallery");
+                  pickImage(source: ImageSource.gallery).then((value) {
+                    if (value != '') {
+                      imageCropperView(value, context).then((value) {
+                        if (value != '') {
+                          Navigator.push(
+                            context,
+                            CupertinoDialogRoute(
+                              builder: (_) => RecognizePage(
+                                path: value,
+                              ),
+                              context: context,
+                            ),
+                          );
+                        }
+                      });
+                    }
+                  });
+                },
+                buttonText: 'Choose from Gallery',
+              ),
+              danfoss.FrontPageButton(
+                onPressed: () {
+                  showDialog(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return AlertDialog(
+                        contentPadding:
+                            EdgeInsets.zero, // Removes default padding
+                        content: Container(
+                          width: 700,
+                          height: 150,
+                          child: Stack(
+                            children: [
+                              Positioned(
+                                top: 8,
+                                right: 8,
+                                child: IconButton(
+                                  icon: Icon(Icons.close),
+                                  onPressed: () {
+                                    Navigator.of(context).pop();
+                                  },
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.all(
+                                    16.0), // Adds padding inside dialog
+                                child: Column(
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: [
+                                    SizedBox(
+                                        height:
+                                            40), // Adds space between close button and text field
+                                    TextField(
+                                      controller: TextEditingController(),
+                                      autofocus: true,
+                                      decoration: InputDecoration(
+                                        hintText: 'Insert serial number...',
+                                        border: OutlineInputBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(8),
+                                          borderSide: BorderSide(
+                                              color: Colors.redAccent),
+                                        ),
+                                        prefixIcon: Icon(Icons.search),
+                                      ),
+                                      textInputAction: TextInputAction.search,
+                                      onSubmitted: (value) {
+                                        Navigator.push(
+                                          context,
+                                          CupertinoDialogRoute(
+                                            builder: (_) => RecognizePage(
+                                              serial: value,
+                                            ),
+                                            context: context,
+                                          ),
+                                        );
+                                      },
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      );
+                    },
+                  );
+                },
+                buttonText: 'Add Manually',
+              ),
+              danfoss.FrontPageButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => HistoryPage()),
+                  );
+                },
+                buttonText: 'Search History',
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
